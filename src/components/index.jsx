d06ef3873c61433255bb6846f32a2614711fa0b0
qmsalas <qmsalas@iastate.edu>
2018-10-19 22:53:44 -0500

    refactor. update npm scripts

---
+++ src/components/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Components
+// import Search from './search/search'
+
+const App = () => <div>Test Content</div>;
+
+ReactDOM.render(<App />, document.querySelector('.root'));
